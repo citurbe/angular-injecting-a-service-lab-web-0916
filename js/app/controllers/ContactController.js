6155cc2a3a4cc0c44cc9778d41ffc468da906a84
Craig Iturbe <citurbe@gmail.com>
2017-01-12 10:39:50 -0500

    Done.

--- js/app/controllers/ContactController.js
+++ js/app/controllers/ContactController.js
@@ -1,5 +1,8 @@
-function ContactController($scope) {
-	$scope.name = 'Bill Gates';
+function ContactController($scope, $timeout) {
+	$scope.name = 'Craig';
+	$timeout(function(){
+		$scope.name = 'Craig Iturbe';
+	}, 5000);
 }
 
 angular
